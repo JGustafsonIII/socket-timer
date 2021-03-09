826e72d0b78e984d19e6723843485dfd5d69e162
John Gustafson <John.Gustafson1@Marist.edu>
2021-03-09 16:41:34 -0500

    Made a simple chat application

---
+++ server/index.js
@@ -0,0 +1,19 @@
+const app = require('express')();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http, {
+  cors: {
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST'],
+  },
+});
+
+io.on('connection', (socket) => {
+  socket.on('message', ({ name, message }) => {
+    console.log(name, message);
+    io.emit('message', { name, message });
+  });
+});
+
+http.listen(8000, function () {
+  console.log('Listening on port 8000');
+});
